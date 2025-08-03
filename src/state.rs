34bf067887ec5465808ac2f58d1fe50977b455ad
Mohammad-Norouzipour <115860771+Mohammad-Norouzipour@users.noreply.github.com>
2025-08-03 17:03:07 +0330

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -2,8 +2,9 @@ use borsh::{BorshDeserialize, BorshSerialize};
 #[derive(BorshSerialize, BorshDeserialize, Default)]
 pub struct CallAccount {
     pub is_initialized: bool,
+    pub session : String
+    pub state : u8,
     pub pcm16: [u16; 8000],
     pub caller: String,
     pub callee: String,
-    pub state: u8,
 }
